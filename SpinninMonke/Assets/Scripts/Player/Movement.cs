695d2700ce1c3b3f43739c9a5df9f272d43566f3
DOCtorActoAntohich <fairy666death@mail.ru>
2022-03-04 20:52:44 +0300

    Implement smooth movement and rotation

--- SpinninMonke/Assets/Scripts/Player/Movement.cs
+++ SpinninMonke/Assets/Scripts/Player/Movement.cs
@@ -5,77 +5,67 @@ namespace Player
     [RequireComponent(typeof(Rigidbody))]
     public class Movement : MonoBehaviour
     {
-        public float acceleration;
         public float maxSpeed;
+
+        [Range(0.0f, 0.99f)]
+        public float velocitySmoothness;
+
+        public float rotationSpeed;
+
+        private const float MaxVelocitySmoothness = 1.0f;
 
         private ThirdPersonCamera _camera;
         private Rigidbody _rigidbody;
-        
-        private Vector2 _movementDirection;
+
+        private Vector3 _movementDirection;
+        private bool ShouldMove => _movementDirection != Vector3.zero;
+
+        private float VelocitySmoothness => MaxVelocitySmoothness - velocitySmoothness;
+        private float RotationStep => Time.deltaTime * rotationSpeed;
 
         private void Start()
         {
             _camera = FindCamera();
             _rigidbody = GetComponent<Rigidbody>();
 
-            _movementDirection = Vector2.zero;
+            _movementDirection = Vector3.zero;
         }
         
         private void Update()
         {
-            HandleMovementKeys();
+            _movementDirection = GetMovementDirection();
+            SmoothMove();
+            if (ShouldMove)
+            {
+                SmoothRotate();
+            }
         }
 
-        private void HandleMovementKeys()
+        private Vector3 GetMovementDirection()
         {
-            UpdateMovementDirection();
-
-            var velocityLeftRight       = _movementDirection.x * _camera.GetRight();
-            var velocityForwardBackward = _movementDirection.y * _camera.GetForward();
-            var netVelocityDirection = (velocityLeftRight + velocityForwardBackward).normalized;
-            _rigidbody.velocity = maxSpeed * netVelocityDirection;
+            var forward = Input.GetAxis("Horizontal") * _camera.GetForward();
+            var right   = Input.GetAxis("Vertical")   * _camera.GetRight();
+            
+            var movementDirection = forward + right;
+            return movementDirection.magnitude > 1 ?
+                movementDirection.normalized :
+                movementDirection;
         }
 
-        private void UpdateMovementDirection()
+        private Vector3 MovementVelocity => maxSpeed * _movementDirection;
+        
+        private void SmoothMove()
         {
-            // Cringe but Input.GetAxis() works poorly due to
-            // Smoothing for controllers (even though I have no controllers ;;).
-            // Tried but was unable to make it as responsive as the following "masterpiece".
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                _movementDirection.x -= 1;
-            }
-            if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
-            {
-                _movementDirection.x += 1;
-            }
-            
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                _movementDirection.x += 1;
-            }
-            if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-            {
-                _movementDirection.x -= 1;
-            }
-            
-            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                _movementDirection.y -= 1;
-            }
-            if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-            {
-                _movementDirection.y += 1;
-            }
-            
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                _movementDirection.y += 1;
-            }
-            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
-            {
-                _movementDirection.y -= 1;
-            }
+            var targetVelocity = MovementVelocity;
+            _rigidbody.velocity = Vector3.Lerp(_rigidbody.velocity, targetVelocity, VelocitySmoothness);
+        }
+
+        private void SmoothRotate()
+        {
+            var newDirection = Vector3.RotateTowards(
+                transform.forward, _movementDirection, 
+                RotationStep, 0);
+            transform.rotation = Quaternion.LookRotation(newDirection);
         }
         
         private static ThirdPersonCamera FindCamera()
